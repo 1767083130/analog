b817c29b9360ff0ea073dd51b621fe772455ef64
swm_work <zyy_doit_01@sina.com>
2018-01-23 10:55:24 +0800

    scoket.io服务器改版0.0.1--创建了ws远程连接，只是用起来问题很多。

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 'use strict';
+
 const app = require('./index');
 const http = require('http');
+const curDomain = '192.168.0.101';
+const port = process.env.PORT || 4000;
 
 //socket model
 const socServer = require('./lib/server/server');
@@ -12,13 +15,13 @@ module.exports = app;
 let server = http.createServer(app);
 
 
-server.listen(process.env.PORT || 4000);
+server.listen(port,curDomain);
 
 //create socket service for get server instance
 socServer.server(server);
 
 server.on('listening', function () {
-    console.log('Server listening on http://localhost:%d', this.address().port);
+    console.log('Server listening on http://%s:%d', curDomain , this.address().port);
 });
 
 process.on('uncaughtException', function(e) {
