d6fbcbb24221297ea7d48caa4521f3517faa81bf
wrd <wangridan9@163.com>
2018-01-23 10:51:18 +0800

    wrd-注册(2)

--- controllers/login/index.js
+++ controllers/login/index.js
@@ -12,23 +12,74 @@ module.exports = function (router) {
         res.render('login', model);
     });
 
+
+    
+    //register
+    router.route("/register").get(function(req,res){
+        res.render("register",{title:'user register'});
+    }).post(function(req,res){
+        
+        var user = global.Promise.getModel('user');
+        var uname = req.body.uname;
+        var upwd = req.body.upwd;
+        User.findOne({username:uname},function(err,doc){
+            if(err){
+                res.send(500);
+
+                req.session.error = '网络异常错误!';
+                console.log(err);
+            }else if(doc){
+                req.session.error = '用户名已存在!';
+                res.send(500);
+            }else{
+                user.create({
+                    username: uname,
+                    password: upwd
+                },function(err,doc){
+                    if(err){
+                        res.send(500);
+                        console.log(err);
+                    }else{
+                        req.session.error = '用户名创建成功！';
+                        res.send(200);
+                    }
+                });
+            }
+        });
+    });
     
 
-    //register
-    router.get('/', function (req, res,next) {
+    //test
+    router.get('/test', function (req, res) {
         let model = {};
         //Include any error messages that come from the login process.
         model.messages = req.flash('error');
-        res.render('register', model);
-        next();
+        res.render('test', model);
     });
 
-    router.post('/', function (req, res) {
+    router.post('/test', function (req, res) {
         // let model = {};
         // //Include any error messages that come from the login process.
         // model.messages = req.flash('error');
-        res.render('register', {});
+        res.render('test', {});
     });
+
+    //register
+    // router.get('/register', function (req, res,next) {
+    //     let model = {};
+    //     //Include any error messages that come from the login process.
+    //     model.messages = req.flash('error');
+    //     res.render('register', model);
+    //     next();
+    // });
+
+    // router.post('/register', function (req, res) {
+    //     // let model = {};
+    //     // //Include any error messages that come from the login process.
+    //     // model.messages = req.flash('error');
+    //     res.render('register', {});
+    // });
+    
     /**
      * Receive the login credentials and authenticate.
      * Successful authentications will go to /profile or if the user was trying to access a secured resource, the URL
--- index.js
+++ index.js
@@ -7,9 +7,47 @@ const app = require('express')(),
     join = require('path').join,
     express = require('express');
 
+ var session = require('express-session');
+ var cookieParser = require('cookie-parser');
+ 
+ //新增multer、mongoose
+//  var multer = require('multer');  
+//  var mongoose = require('mongoose');  
+//  global.database = require('./lib/database');  
+//  global.db = mongoose.connect("mongodb://localhost:27017/Stock");  
+
+//bodyParser模块来做文件解析，将表单里的数据进行格式化
+var bodyParser = require("body-parser"); 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));  
+// app.use(multer());  
+app.use(cookieParser());  
+
+
+//引用express-session  
+app.use(session({
+    secret:'secret',
+      cookie:{
+          maxAge:1000*60*30
+      }
+}));
+
+app.use(function(req,res,next){
+    res.locals.user = req.session.user;
+    var err = req.session.error;
+    delete req.session.error;
+    res.locals.message = "注册成功！";
+    if(err){
+        res.locals.message = '注册失败！';
+    }
+    next();     //中间件传递
+});
+
+
 app.use(kraken(options));
 app.use('/assets',express.static('assets'));
 app.use('/assets',express.static('public'));
+
 
 nunjucks.configure('./public/templates', {
     autoescape: true,
